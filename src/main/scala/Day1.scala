d29b1bdccba620df66034a9f9f833f192b9f5aba
Aaron Bush <asb.bush@gmail.com>
2018-12-06 15:29:28 -0500

    day1 first part

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -1,9 +1,15 @@
+import scala.io.Source
+
 class Day1 {
 
 }
 
 object Day1 {
+  val fileName = "src/main/resources/Day1-input.txt"
+
   def main(args: Array[String]): Unit = {
     println("Hi")
+    println(Source.fromFile(fileName).getLines().map((s: String) => s.toInt).reduce((x1: Int, x2: Int) => x1 + x2))
+    println(Source.fromFile(fileName).getLines().map((s: String) => s.toInt).sum)
   }
 }
